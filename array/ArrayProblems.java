841d89ec6236cd611db779b583779a266e8f89d1
Alex Trzeciak <trebek08@gmail.com>
2018-01-10 18:04:57 -0800

    create directory for array problems

---
+++ array/ArrayProblems.java
@@ -0,0 +1,10 @@
+// Array Notes 
+// good for retrieving and updating (O(1))
+// deleting is simply moving all over one to the left O(n - i)
+// A[i] denotes i + 1 element 
+// resizing affects worst case time of insertion 
+
+
+// 6.1 The dutch national flag problem 
+
+ArrayProblems
