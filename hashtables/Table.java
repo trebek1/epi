0e86e713bf8854166b1c967aa5931b460c97a87f
Alex Trzeciak <trebek08@gmail.com>
2018-01-19 23:46:43 -0800

    u[master] solved palindrome and construct letter

--- hashtables/Table.java
+++ hashtables/Table.java
@@ -29,28 +29,125 @@ class Table {
 		return anagrams;
 	}
 
+  static boolean isPalindrome(String str){
+
+    Map<Character, Integer> table = new HashMap<>();
+
+    for(int i = 0; i < str.length(); i++){
+      char c = str.charAt(i);
+      if(table.containsKey(c)){
+        table.put(c, table.get(c) + 1);
+      } else {
+        table.put(c, 1);
+      }
+    }
+
+    if(str.length() % 2 == 0){
+      // even
+      for(Character c : table.keySet()){
+        int val = table.get(c);
+        if(val % 2 != 0){
+          return false;
+        }
+      }
+      return true;
+
+    } else {
+      int odds = 0; 
+      for(Character c : table.keySet()){
+        int val = table.get(c);
+        if(val % 2 != 0){
+          odds++;
+          if(odds != 1){
+            return false;
+          } 
+        }
+      }
+    }
+
+    return true;
+
+  }
+
+  static boolean constructLetter(String letter, String magazine){
+
+    Map<Character, Integer> index = new HashMap<>();
+
+    // Adding Letters to the Map
+    for(int i = 0; i < letter.length(); i++){
+      char c = letter.charAt(i);
+      if(index.containsKey(c)){
+        index.put(c, index.get(c) + 1);
+      } else {
+        index.put(c, 1);
+      }
+    }
+
+    // remove letters from index until empty 
+    // If empty return true
+
+    for(int i = 0; i < magazine.length(); i++){
+      
+      char c = magazine.charAt(i);
+      if(index.containsKey(c)){
+        int val = index.get(c);
+        if(val == 1){
+      
+          // delete key from Map
+          index.remove(c);
+        } else {
+          index.put(c, val - 1);
+        }
+      }
+      if(index.isEmpty()){
+        return true;
+      }
+    }
+    
+
+
+
+    return false;
+  }
+
   public static void main(String[] args){
   	// 13.1 Partition into anagrams
 
-  	List<String> dictionary = new ArrayList<>();
+    	// List<String> dictionary = new ArrayList<>();
 
-  	dictionary.add("debitcard");
-  	dictionary.add("badcredit");
-  	dictionary.add("elvis");
-  	dictionary.add("silent");
-  	dictionary.add("lives");
-  	dictionary.add("freedom");
-  	dictionary.add("listen");
-  	dictionary.add("levis");
-  	dictionary.add("money");
+    	// dictionary.add("debitcard");
+    	// dictionary.add("badcredit");
+    	// dictionary.add("elvis");
+    	// dictionary.add("silent");
+    	// dictionary.add("lives");
+    	// dictionary.add("freedom");
+    	// dictionary.add("listen");
+    	// dictionary.add("levis");
+    	// dictionary.add("money");
 
-  	List<List<String>> anagrams = findAnagrams(dictionary);
-  	for(List<String> list : anagrams){
+    	// List<List<String>> anagrams = findAnagrams(dictionary);
+    	// for(List<String> list : anagrams){
 
-  		for(String s : list){
-  			System.out.print(s + " ");
-  		}
-  		System.out.println("");
-  	}
+    	// 	for(String s : list){
+    	// 		System.out.print(s + " ");
+    	// 	}
+    	// 	System.out.println("");
+    	// }
+
+
+    // 13.2 Is the String a Palindrome
+
+      // System.out.println(isPalindrome("racecar"));
+      // System.out.println(isPalindrome("alex"));
+
+    // 13.3 Is An Anonymous Letter Constructable ? 
+      String letter = "a joy to all";
+      String magazine1 = " the bear was a joy to all";
+      String magazine2 = " xbox is the best";
+
+      System.out.println(constructLetter(letter, magazine1));
+      System.out.println(constructLetter(letter, magazine2));
+
+
   }
 }
